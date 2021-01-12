f1d08048b67853169e3fed09d50ae2df751c466e
Steve Hill <sghill.dev@gmail.com>
2021-01-12 14:06:58 -0800

    Only run the featureRuntimeOnly test for 6.7+
    This support was added in Gradle 6.7
    
    JENKINS-58205

--- src/test/groovy/org/jenkinsci/gradle/plugins/manifest/GeneratePluginDependenciesManifestTaskIntegrationSpec.groovy
+++ src/test/groovy/org/jenkinsci/gradle/plugins/manifest/GeneratePluginDependenciesManifestTaskIntegrationSpec.groovy
@@ -1,9 +1,12 @@
 package org.jenkinsci.gradle.plugins.manifest
 
 import org.gradle.testkit.runner.TaskOutcome
+import org.gradle.util.GradleVersion
 import org.jenkinsci.gradle.plugins.jpi.IntegrationSpec
 import org.jenkinsci.gradle.plugins.jpi.TestDataGenerator
 import org.jenkinsci.gradle.plugins.jpi.TestSupport
+import spock.lang.IgnoreIf
+import spock.lang.Issue
 import spock.lang.Unroll
 
 import static org.jenkinsci.gradle.plugins.jpi.TestSupport.ant
@@ -81,14 +84,62 @@ class GeneratePluginDependenciesManifestTaskIntegrationSpec extends IntegrationS
         'runtimeOnly'       | ant('1.10')        | ant('1.11')        | TaskOutcome.SUCCESS
         'antApi'            | ant('1.10')        | ant('1.11')        | TaskOutcome.SUCCESS
         'antImplementation' | ant('1.10')        | ant('1.11')        | TaskOutcome.SUCCESS
-        'antRuntimeOnly'    | ant('1.10')        | ant('1.11')        | TaskOutcome.SUCCESS
         // non-plugin changes shouldn't force this to rerun
         'api'               | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
         'implementation'    | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
         'runtimeOnly'       | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
         'antApi'            | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
         'antImplementation' | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
-        'antRuntimeOnly'    | log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
     }
 
+    @Unroll
+    @Issue('https://github.com/gradle/gradle/issues/13278')
+    @IgnoreIf({ getGradleVersionForTest() < GradleVersion.version('6.7') })
+    def 'should rerun only if #config plugin dependencies change 6.7+'(String before, String after, TaskOutcome secondRun) {
+        given:
+        build.text = """\
+            $BUILD_FILE
+            java {
+                registerFeature('ant') {
+                    usingSourceSet(sourceSets.main)
+                }
+            }
+            dependencies {
+                antRuntimeOnly $before
+            }
+            """.stripIndent()
+
+        when:
+        def result = gradleRunner()
+                .withArguments(taskName)
+                .build()
+
+        then:
+        result.task(taskPath).outcome == TaskOutcome.SUCCESS
+
+        when:
+        build.text = """\
+            $BUILD_FILE
+            java {
+                registerFeature('ant') {
+                    usingSourceSet(sourceSets.main)
+                }
+            }
+            dependencies {
+                antRuntimeOnly $after
+            }
+            """.stripIndent()
+        def rerunResult = gradleRunner()
+                .withArguments(taskName)
+                .build()
+
+        then:
+        rerunResult.task(taskPath).outcome == secondRun
+
+        where:
+        before             | after              | secondRun
+        ant('1.10')        | ant('1.11')        | TaskOutcome.SUCCESS
+        // non-plugin changes shouldn't force this to rerun
+        log4jApi('2.13.0') | log4jApi('2.14.0') | TaskOutcome.UP_TO_DATE
+    }
 }
